3ec2e787a0a4cf1e7a5ad033aaa652a71f3f92a9
Mathis Detourbet <mathis.detourbet@gmail.com>
2014-02-07 21:06:39 +0100

    test

---
+++ TicTacToeProject/TicTacToeProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TicTacToeProject
+//
+//  Created by Mathis Detourbet on 07/02/2014.
+//  Copyright (c) 2014 Mathis Detourbet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
